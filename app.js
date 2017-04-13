892f55d0cd4b4fc7cd926eeb6faf447827a49fac
David George <david.george@uk.ibm.com>
2017-04-13 11:11:03 +0000

    Added mqlight connection

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+'use strict';
+
 // Configure dotenv to get environment variables
 require('dotenv').config();
+
+var messagehubConfig = require('./messagehub');
 
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
@@ -25,42 +29,46 @@ var conversation = new ConversationV1({
   version_date: '2017-02-03'
 });
 
-// Enable body parsing
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+messagehubConfig().then(setup);
 
-// Serve the files out of ./public as our main files
-app.use(express.static(__dirname + '/public'));
+function setup (mqlightClient) {
 
-// Get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
+  // Enable body parsing
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
 
+  // Serve the files out of ./public as our main files
+  app.use(express.static(__dirname + '/public'));
 
-var messageReceived = function (message, fn) {
-  // Setup payload
-  var payload = {
-    workspace_id: process.env.WORKSPACE_ID,
-    context: message.context,
-    input: message.input
+  // Get the app environment from Cloud Foundry
+  var appEnv = cfenv.getAppEnv();
+
+  var messageReceived = function (message, fn) {
+    // Setup payload
+    var payload = {
+      workspace_id: process.env.WORKSPACE_ID,
+      context: message.context,
+      input: message.input
+    };
+
+    // Send message to Conversation service
+    conversation.message(payload,
+      function (err, data) {
+        if (err) {
+          return io.emit('message', err);
+        }
+        return fn(data);
+      }
+    );
   }
 
-  // Send message to Conversation service
-  conversation.message(payload,
-    function(err, data) {
-      if (err) {
-        return io.emit('message', err);
-      }
-      return fn(data);
-    }
-  );
+  // Start socket.io
+  io.on('connection', function (socket) {
+    socket.on('message', messageReceived);
+  });
+
+  // Start server
+  http.listen(appEnv.port, '0.0.0.0', function() {
+    console.log("server starting on " + appEnv.url);
+  });
 }
-
-// Start socket.io
-io.on('connection', function (socket) {
-  socket.on('message', messageReceived);
-});
-
-// Start server
-http.listen(appEnv.port, '0.0.0.0', function() {
-  console.log("server starting on " + appEnv.url);
-});
---
+++ messagehub.js
@@ -0,0 +1,76 @@
+'use strict';
+
+// cfenv provides access to your Cloud Foundry environment
+// for more info, see: https://www.npmjs.com/package/cfenv
+var cfenv = require('cfenv');
+var mqlight = require('mqlight');
+var uuid = require('node-uuid');
+var winston = require('winston');
+
+var SERVICE_NAME = 'messagehub';
+
+// Get the app environment from Cloud Foundry
+var appEnv = cfenv.getAppEnv();
+
+// Establish credentials
+var opts = {};
+
+var service = appEnv.getService(SERVICE_NAME);
+
+if (service) {
+  opts.service = service.credentials.mqlight_lookup_url;
+  opts.user = service.credentials.user;
+  opts.password = service.credentials.password;
+
+  if (!opts.hasOwnProperty('service') ||
+      !opts.hasOwnProperty('user') ||
+      !opts.hasOwnProperty('password')) {
+    throw new Error('Error - Check that app is bound to service');
+  }
+}
+else if (process.env.MQLIGHT_LOOKUP_URL &&
+           process.env.MQLIGHT_USER &&
+           process.env.MQLIGHT_PASSWORD) {
+  opts.service = process.env.MQLIGHT_LOOKUP_URL;
+  opts.user = process.env.MQLIGHT_USER;
+  opts.password = process.env.MQLIGHT_PASSWORD;
+} else {
+  throw new Error('No mqlight service is bound or configured');
+}
+
+opts.id = 'TUTORIALBOT_' + uuid.v4().substring(0, 7);
+
+// Message Handler
+function handleMessage (data, delivery) {
+  winston.info('handleMessage', JSON.parse(data));
+}
+
+function config () {
+  return new Promise (function (resolve, reject) {
+
+    winston.info('config', opts);
+
+    var mqlightClient = mqlight.createClient(opts, function (err) {
+      if (err) {
+        winston.error('Connection to ' + opts.service + ' using client-id ' +
+          mqlightClient.id + ' failed: ' + err);
+        reject(err);
+      } else {
+        winston.info('Connected to ' + opts.service + ' using client-id ' +
+          mqlightClient.id);
+      }
+
+      mqlightClient.on('message', handleMessage);
+
+      mqlightClient.on('error', function (err) {
+        winston.error(err);
+      });
+
+      resolve(mqlightClient);
+
+    });
+
+  });
+}
+
+module.exports = config;
